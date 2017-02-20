25185e546be2bc4bd0dc3ba0bfba59aa260b4906
Giuseppe Guarnieri <g.guarnieri@reply.it>
2017-02-20 10:29:51 +0100

    Amendments to main

--- src/main/java/com/reply/spartans/datacenter/Main.java
+++ src/main/java/com/reply/spartans/datacenter/Main.java
@@ -8,16 +8,15 @@ import java.util.Scanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static com.google.common.base.Preconditions.*;
 import com.reply.spartans.datacenter.model.ProblemParameters;
 import com.reply.spartans.datacenter.model.Solution;
-
-import eu.reply.hashcode2017.codespartans.Main;
 
 public class Main {
 
 	private static final Logger log = LoggerFactory.getLogger(Main.class);
-	
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws Exception {
 		if (args.length != 2) {
 			log.error("Needs exactly two arguments: input file and output file");
 			return;
@@ -26,7 +25,7 @@ public class Main {
 		final ProblemParameters parameters = Main.readInputParametersFrom(args[0]);
 
 		final Solution result = Main.processSolution(parameters);
-		
+
 		Main.renderOutput(result, parameters);
 	}
 
@@ -41,16 +40,15 @@ public class Main {
 
 	private static ProblemParameters readInputParametersFrom(String filename)
 			throws FileNotFoundException, IOException {
-		Preconditions.checkArgument(Preconditions.checkNotNull(filename).trim().isEmpty() == false,
+		checkArgument(checkNotNull(filename).trim().isEmpty() == false,
 				"You must supply an input file name");
 
 		ProblemParameters problemParameters = null;
 		try (final Scanner inputData = new Scanner(FileSystems.getDefault().getPath(filename), "UTF-8")) {
-			
+
 			problemParameters = new ProblemParameters();
 		}
 		return problemParameters;
 	}
-
 
 }

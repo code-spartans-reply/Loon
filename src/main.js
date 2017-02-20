9c87d7e03623143046f3b1e8376283fdb9e5a7a4
prashmi9 <prashmi.89@gmail.com>
2017-02-20 12:00:46 +0000

    Function to check if largest spot available

---
+++ src/main.js
@@ -0,0 +1,15 @@
+function maxServer(){
+    var spotsCapacity = [10, 5, 5, 2, 1]; // max size available
+    var server = [ 0, 1, 2, 3 ,4 , 5 ]; // servers
+    var getMax = 0;
+    for( i=0; i < spotsCapacity.length; i++){
+
+        if(spotsCapacity[i] > server[i])
+        {
+            getMax = Math.max(spotsCapacity[i], server[i]);
+        }
+        console.log("assign to max ", getMax);
+    }
+    return getMax;
+}
+maxServer();
